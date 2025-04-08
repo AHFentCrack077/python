4ac4a6187e5077ecee03ac2a2cc9eb2d7ab43cba
adolf <mneraihianr@gmail.com>
2025-04-09 10:21:39 +1200

    added input statments

--- test2.py/test2.py
+++ test2.py/test2.py
@@ -1,3 +1,4 @@
-print ("hello world")
-print ("yay")
-print ("new")
+Name = input ("What is your name: ")
+age = input ("What is your age: ")
+
+activity_choice = ["1. Music jam session", "2. Science Experment Lab", "3. Sports Leadership Training"]
