9e78fbaad2d2b20743ba0d129a3a32a4cf06bdb7
adolf <mneraihianr@gmail.com>
2025-04-09 10:27:36 +1200

    added a print statment

--- test2.py/test2.py
+++ test2.py/test2.py
@@ -2,3 +2,9 @@ Name = input ("What is your name: ")
 age = input ("What is your age: ")
 
 activity_choice = ["1. Music jam session", "2. Science Experment Lab", "3. Sports Leadership Training"]
+
+print('choose an activity:')
+print(f'{activity_choice[0]}')
+print(f'{activity_choice[1]}')
+print(f'{activity_choice[2]}')
+activity = input("enter the number of your choosen activity: ")
