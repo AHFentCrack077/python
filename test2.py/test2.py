50ae645d4ecb7b2d925bcf2fd63b6f3978cdf70f
Eraihia <mneraihianr@gmail.com>
2025-05-02 13:08:11 +1200

    age verifyer

--- test2.py/test2.py
+++ test2.py/test2.py
@@ -1,12 +1,20 @@
 print ("hello")
 Name = input ("What is your name: ")
 age = input ("What is your age: ")
-
+age = int(age)
 program_choice = ["1. Cultural immersion", "2. Kayaking and pancakes", "3. Mountain biking"]
+if age > 17:
+    print("You cant go becouse you are to old.")
+elif 5 <= age <= 17:
+    print("You can go.")
+else:
+    print("you must be at least 5 years old to go.")
 
 print('choose an activity:')
 print(f'{program_choice[0]}')
 print(f'{program_choice[1]}')
 print(f'{program_choice[2]}')
 activity = input("enter the number of your choosen activity: ")
+
+
 
