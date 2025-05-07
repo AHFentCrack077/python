adcba6262bf0afd679a51fe230892677223e75c8
Eraihia <mneraihianr@gmail.com>
2025-05-08 11:41:44 +1200

    restarted and added a age verifier

--- test2.py/test2.py
+++ test2.py/test2.py
@@ -1,20 +1,21 @@
-print ("hello")
-Name = input ("What is your name: ")
-age = input ("What is your age: ")
-age = int(age)
-program_choice = ["1. Cultural immersion", "2. Kayaking and pancakes", "3. Mountain biking"]
-if age > 17:
-    print("You cant go becouse you are to old.")
-elif 5 <= age <= 17:
-    print("You can go.")
-else:
-    print("you must be at least 5 years old to go.")
+print("Hello and welcome! ")
 
-print('choose an activity:')
-print(f'{program_choice[0]}')
-print(f'{program_choice[1]}')
-print(f'{program_choice[2]}')
-activity = input("enter the number of your choosen activity: ")
+camper_name = input("Enter your name")
+
+camper_age = 0
+while not (5 <= camper_age <= 17):
+    try:
+        age_input = input("Please enter the camper's age (5-17): ")
+        camper_age = int(age_input)
+        if not (5 <= camper_age <= 17):
+            print("Invalid age. Campers must be between 5 and 17 years old.")
+    except ValueError:
+        print("Invalid input. Please enter a number for the age.")
+
+
+
+
+
 
 
 
